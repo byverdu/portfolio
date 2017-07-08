2c7a5cd1ddcb6fbc0c22a48b70cb6ab0807264b5
albert Byverdu :) <byverdu@gmail.com>
2017-07-08 18:58:20 +0100

    tests for lettering method

--- app/src/__tests__/utils.test.js
+++ app/src/__tests__/utils.test.js
@@ -4,21 +4,54 @@ import chai from 'chai';
 const expect = chai.expect;
 
 const splitText = ( sentence ) => {
-		return sentence.split( ' ' )
-			.map( item => item.split( '' ));
-	};
+  return sentence.split( ' ' )
+    .map( item => item.split( '' ));
+};
+
+const createElements = ( letters ) => {
+		let count = 0;
+		const result = [];
+		while ( count < letters.length ) {
+			const span = document.createElement( 'span' );
+			span.classList.add( `group-${count}` );
+			const temp = letters[ count ].map(( item, index ) => {
+				const bTag = document.createElement( 'b' );
+				bTag.textContent = item;
+				bTag.classList.add( `char-${index}` );
+				return bTag;
+			});
+			temp.forEach( elem => span.appendChild( elem ))
+			result.push( span );
+			count ++;
+		}
+		return result;
+	}
 
 describe( 'lettering', () => {
+  let sentence;
+  let letters;
+  let spanTags;
+  beforeEach(() => {
+    sentence = 'react rocks';
+  });
+
   it( 'is defined', () => {
     expect( lettering ).not.eq( undefined );
     expect( lettering ).to.be.a( 'Function' );
   });
 
   it( 'should split text', () => {
-    const sentence = 'react rocks';
-    expect( splitText ).not.eq( undefined );
+    letters = splitText( sentence );
     expect( splitText ).to.be.a( 'Function' );
     expect( splitText( sentence ))
       .to.eql([ [ "r","e","a","c","t" ],[ "r","o","c","k","s" ] ]);
+  });
+
+  it( 'is creates as much elements as words in sentence', () => {
+    spanTags = createElements( letters );
+    expect( createElements ).to.be.a( 'Function' );
+    expect( spanTags ).to.have.length( 2 );  
+    expect( spanTags[ 0 ]).to.be.instanceof( HTMLSpanElement );  
+    expect( spanTags[ 0 ].firstElementChild ).to.be.instanceof( HTMLElement );
   });
 });
--- app/src/utils/index.js
+++ app/src/utils/index.js
@@ -1,3 +1,31 @@
-export function lettering() {
+export function lettering( htmlElement ) {
+  const splitText = ( sentence ) => {
+  return sentence.split( ' ' )
+    .map( item => item.split( '' ));
+};
 
+const createElements = ( letters ) => {
+		let count = 0;
+		const result = [];
+		while ( count < letters.length ) {
+			const span = document.createElement( 'span' );
+			span.classList.add( `group-${count}` );
+			const temp = letters[ count ].map(( item, index ) => {
+				const bTag = document.createElement( 'b' );
+				bTag.textContent = item;
+				bTag.classList.add( `char-${index}` );
+				return bTag;
+			});
+			temp.forEach( elem => span.appendChild( elem ))
+			result.push( span );
+			count ++;
+		}
+		return result;
+	}
+
+  const text = splitText( htmlElement.textContent );
+	const tempSpans = createElements( text );
+	
+	htmlElement.innerHTML = '';
+	tempSpans.forEach( item => htmlElement.appendChild( item ));
 }
