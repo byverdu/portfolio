f94423c8b10caa2eb443d730c0f58da8954b16c7
albert Byverdu :) <byverdu@gmail.com>
2017-07-19 01:05:23 +0100

    working example for post in server

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 import Express from 'express';
 import path from 'path';
+const bodyParser = require( 'body-parser' );
 const app = Express();
 
 // Express settings
 const port = process.env.port || 3000;
-app.use('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'app/public/index.html'));
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(Express.static(path.join(__dirname, 'app/build')))
+
+app.post('/', (req, res) => {
+  console.log(req,res)
+  res.sendFile(path.join(__dirname, 'app/build/xoxo.html'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'app/build/index.html'));
 });
 
 app.listen(`${port}`, () => {
