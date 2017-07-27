358d8f7bfe712db01593950e72ca2a43335c792f
albert Byverdu :) <byverdu@gmail.com>
2017-07-27 22:07:15 +0100

    moved shared styles for error and success to own file

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ import path from 'path';
 const bodyParser = require( 'body-parser' );
 const app = Express();
 import { smtpConfig, emailOpts } from './emailConfig';
-	
+import ReactDOMServer from 'react-dom/server'
+
 // Express settings
-const port = process.env.port || 3000;
+const port = process.env.port || 9090;
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(Express.static(path.join(__dirname, 'app/build')));
 
 // route handler
 app.get('/', (req, res) => {
+  const renderedApp = ReactDOMServer.renderToString(element);
   res.sendFile(path.join(__dirname, 'app/build/index.html'));
 });
 
