37939a284cdeb801f6a41b0c05046f97126032aa
byverdu <byverdu@gmail.com>
2018-09-18 22:03:27 +0100

    content component refactored

--- app/src/components/content/index.js
+++ app/src/components/content/index.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { tileBuilder } from '../../builders';
 import './Content.css'
 
-export default class Content extends Component {
-  render() {
-    return (
-      <section
-        id="content"
-        className="portfolio__content">
-				{tileBuilder}
-			</section>
-    );
-  }
-}
+const Content = () => (
+  <section
+    id="content"
+    className="portfolio__content">
+    {tileBuilder}
+  </section>
+);
+
+export default Content;
